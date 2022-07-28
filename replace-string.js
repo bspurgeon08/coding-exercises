4d2833341ec56825353b60f644453ff9a0f15c0a
bspurgeon08 <betty.spurgeon@wasdi.cloud>
2022-07-28 14:41:15 +0200

    Replace string function complete

--- replace-string.js
+++ replace-string.js
@@ -2,8 +2,11 @@
 
 //Function should take 3 elements: the string, the part to replace, the replacement
 const strReplace = (sStr, sDelete, sReplacement) =>{ 
-    let sReturnStr = sStr.map(sElement => {
-        return sElement.split(substr).join(newstr);
-    })
+    return sStr.split(" ").map(sElement => {
+        return sElement === sDelete ? sReplacement  : sElement
+    }).join(" ")
     //return the string
+ 
 }
+
+console.log(strReplace("I am a dog", "dog", "cat"));
